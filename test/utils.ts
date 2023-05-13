2de3c5e5e855fcd56aa482719885a54da6950d6d
xryuseix <ryusei143.shootingstar@gmail.com>
2023-05-13 21:17:01 +0900

    chore: libraries version

--- test/utils.ts
+++ test/utils.ts
@@ -7,20 +7,19 @@ import firebase from "firebase/compat/app";
 import { getConverter, WithId } from "@/lib/firebase";
 import { DocumentData } from "firebase/firestore";
 
-
 let testEnv: RulesTestEnvironment;
 export const initializeTestEnvironment = async (projectId?: string) => {
   process.env.VITE_EMULATORS = "true";
   process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
   testEnv = await _initializeTestEnvironment({
-    projectId: projectId ?? "fs-sample-open-chat-test",
+    projectId: projectId ?? "ryu-se-test",
     firestore: {
       rules: readFileSync("firestore.rules", "utf8"),
     },
     hub: {
       host: "127.0.0.1",
       port: 4400,
-    }
+    },
   });
 };
 export const getTestEnv = () => testEnv;
