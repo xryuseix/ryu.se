f21db1d7806476b581da674ab7b199401105e7b6
xryuseix <ryusei143.shootingstar@gmail.com>
2023-05-29 05:34:34 +0900

    feat: LinkForm Styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,19 @@
 import { LinkForm } from "./LinkForm";
 import { Links } from "./Links";
 import { Logs } from "./Logs";
+import { Container, Heading, Divider } from "@chakra-ui/react";
 
 export const App = () => {
   return (
-    <div>
-      <h1>URL shortener</h1>
-      <div>
-        <LinkForm />
-        <hr />
-        <Links />
-        <Logs />
-      </div>
-    </div>
+    <Container maxW="container.lg">
+      <Heading as="h1" size="xl" m={4}>
+        URL shortener
+      </Heading>
+      <LinkForm />
+      <Divider my="6" />
+      <Links />
+      <Divider my="6" />
+      <Logs />
+    </Container>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,30 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import { App } from "./components/App";
 import { AuthProvider } from "./contexts/AuthContext";
 import { UsersProvider } from "./contexts/UsersContext";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "@/lib/firebase";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const colors = {
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "#F6F7F9",
+      },
+    },
+  },
+};
+
+const theme = extendTheme(colors);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <UsersProvider>
-        <App />
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
       </UsersProvider>
     </AuthProvider>
   </React.StrictMode>
