026fd089449a752baca00289d3bfd78360493dee
xryuseix <ryusei143.shootingstar@gmail.com>
2023-05-29 07:18:57 +0900

    feat: delete modal

---
+++ src/components/DeleteModal.tsx
@@ -0,0 +1,47 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+} from "@chakra-ui/react";
+
+type PropsT = {
+  isOpen: boolean;
+  onClose: () => void;
+  from: string;
+  to: string;
+  handleDelete: () => void;
+};
+
+export const DeleteModal = (props: PropsT) => {
+  return (
+    <Modal isOpen={props.isOpen} onClose={props.onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Confirmation</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          Do you really want to remove the following link?
+          <br /> {`https://ryuse.dev/${props.from} → ${props.to}`}
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            colorScheme="blue"
+            variant="outline"
+            mr={3}
+            onClick={props.onClose}
+          >
+            Cancel
+          </Button>
+          <Button variant="ghost" onClick={props.handleDelete}>
+            Delete
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
