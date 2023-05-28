8ace2ef59bc137b05711272e9bde32c0e380fa41
xryuseix <ryusei143.shootingstar@gmail.com>
2023-05-29 06:09:13 +0900

    feat: Links Styles

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -3,6 +3,7 @@ import { linksQuery } from "@/lib/link";
 import { LoadingScreen } from "@/components/LoadingScreen";
 import { Link } from "@/components/Link";
 import { useAuth } from "@/contexts/AuthContext";
+import { Box, Table, Thead, Tbody, Tr, Th } from "@chakra-ui/react";
 
 export const Links = () => {
   const { user } = useAuth();
@@ -11,25 +12,30 @@ export const Links = () => {
   if (loading) return <LoadingScreen />;
 
   return (
-    <div id="links" className="links">
-      <table>
-        <thead>
-          <tr>
-            <th>Created</th>
-            <th>From</th>
-            <th>To</th>
-            <th>Updated</th>
-            <th>Expires</th>
-            <th>Remark</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
+    <Box bg={"#FFFFFF"} shadow="md" rounded="md" p={4}>
+      <Table
+        variant="simple"
+        overflowX="scroll"
+        whiteSpace="nowrap"
+        display="block"
+      >
+        <Thead>
+          <Tr>
+            <Th minW={150}>Created</Th>
+            <Th minW={100}>From</Th>
+            <Th minW={200}>To</Th>
+            <Th minW={150}>Updated</Th>
+            <Th minW={150}>Expires</Th>
+            <Th minW={100}>Actions</Th>
+            <Th minW={100}>Remark</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
           {links?.map((link) => (
             <Link key={link.id} link={link} />
           ))}
-        </tbody>
-      </table>
-    </div>
+        </Tbody>
+      </Table>
+    </Box>
   );
 };
