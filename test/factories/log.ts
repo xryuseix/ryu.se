78e8120992abe223c76cd460e79d605ea9ae77af
xryuseix <ryusei143.shootingstar@gmail.com>
2023-06-16 12:05:26 +0900

    fix: deplicated Timestamp in log

--- test/factories/log.ts
+++ test/factories/log.ts
@@ -1,10 +1,9 @@
 import { Factory } from "fishery";
-import { Timestamp } from "firebase/firestore";
 import { AccessLog } from "@/../functions/src/shared/types/log";
 
 export const logFactory = Factory.define<AccessLog>(({ sequence }) => ({
   id: sequence.toString(),
-  accessedAt: Timestamp.fromDate(new Date()),
+  accessedAt: new Date(),
   userId: "",
   from: "not_found_path",
   to: null,
